7d6f90d6a2f252b552f8b5a7b3222a34e0a6d6ce
Marcos Ivan Robles Hernandez <marcos92robles@gmail.com>
2018-10-31 23:42:28 -0600

    MIRH Agregar Metodo Importar al controlador Inventario. Agregar Vista Importar

--- CRUDNetCore/Controllers/InventarioController.cs
+++ CRUDNetCore/Controllers/InventarioController.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CRUDNetCore.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using OfficeOpenXml;
 
 namespace CRUDNetCore.Controllers
 {
@@ -13,9 +16,88 @@ namespace CRUDNetCore.Controllers
     {
         private readonly MapCelTestContext _context;
 
+
         public InventarioController(MapCelTestContext context)
         {
             _context = context;
+        }
+
+
+        public IActionResult Importar()
+        {
+           
+            List<Inventario> ListaInventario = new List<Inventario>();
+            string rootFolder = "C:/Users/Marcos/Documents/Share/";
+            string fileName = @"ImportarInventario.xlsx";
+
+            string celdaExcel, version = String.Empty;
+            FileInfo file = new FileInfo(Path.Combine(rootFolder, fileName));
+
+            using (ExcelPackage package = new ExcelPackage(file))
+            {
+                ExcelWorksheet workSheet = package.Workbook.Worksheets["Inventario"];
+                int totalRows = workSheet.Dimension.Rows;
+                int totalColums = workSheet.Dimension.Columns;
+
+
+                Inventario objInventario = new Inventario();
+                List<String> CamposTabla = new List<string>();
+
+                for (int i = 1; i <= totalRows; i++)
+                {
+                    for (int j = 1; j <= totalColums; j++)
+                    {
+                        //Obtener version de plantilla
+                        if (workSheet.Cells[1, 1].Value.ToString() == "version")
+                        {
+                            version = workSheet.Cells[i, j + 1].Value.ToString();
+                        }
+
+                        //Agregar nombres de campos
+                        if (i == 2)
+                        {
+                            CamposTabla.Add(workSheet.Cells[i, j].Value.ToString());
+                        }
+
+                        //Obtener registros
+                        if (i > 2)
+                        {
+
+                            objInventario.UnqInvinventarioKey = Guid.NewGuid();
+                            if (workSheet.Cells[i, j].Value == null)
+                            {
+                                objInventario.VchSku = "NADA";
+                            }
+                            else { objInventario.VchSku = workSheet.Cells[i, j].Value.ToString(); }
+
+                            if (workSheet.Cells[i, j + 1].Value == null)
+                            {
+                                objInventario.VchNumeroSerie = "NADA";
+                            }
+                            else
+                            {
+                                objInventario.VchNumeroSerie = workSheet.Cells[i, j + 1].Value.ToString();
+                            }
+                            
+
+                            if (workSheet.Cells[i, j + 2].Value == null)
+                            {
+                                objInventario.IntCantidad = 0;
+                            }
+                            else { objInventario.IntCantidad = Convert.ToInt32(workSheet.Cells[i, j + 2].Value); }
+
+                            objInventario.UnqGenproductoLink = Guid.Parse("1F3038BF-01D8-44C9-AA64-533D622289AE");
+
+
+                            _context.Inventario.AddRange(objInventario);
+                            _context.SaveChanges();
+                         
+                            break;
+                        }
+                    }
+                }
+            }
+            return View();           
         }
 
         // GET: Inventario
